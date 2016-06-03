13b90a27583df137e24469bf4807f8256c9a61e1
Liz Favre <egfavre@gmail.com>
2016-06-03 13:26:18 -0400

    multiple image types based on key pressed

--- core/src/com/egfavre/MyGdxGame.java
+++ core/src/com/egfavre/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.egfavre;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,6 +19,13 @@ public class MyGdxGame extends ApplicationAdapter {
 	static final int HEIGHT = 16;
 	static final int FINAL_WIDTH = WIDTH * 5;
 	static final int FINAL_HEIGHT = HEIGHT * 5;
+	float x, y, xv, yv;
+	boolean canJump, faceRight = true;
+	static final float MAX_VELOCITY = 300;
+	static final float MAX_JUMP_VELOCITY = 2000;
+	static final float DECELERATION = 0.95f;
+	static final int GRAVITY = -50;
+
 
 	@Override
 	public void create () {
@@ -34,14 +42,67 @@ public class MyGdxGame extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
+		move();
 		time += Gdx.graphics.getDeltaTime();
 		TextureRegion img;
-		img = down;
+		if (y>0) {
+			img = up;
+		}
+		else if (xv != 0) {
+			img = right;
+		}
+		else {
+			img = left;
+		}
+
+
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0, FINAL_WIDTH, FINAL_HEIGHT);
 		batch.end();
+	}
+
+	public void move(){
+		if(Gdx.input.isKeyPressed(Input.Keys.UP) && canJump){
+			yv = MAX_JUMP_VELOCITY;
+			canJump = false;
+		}
+		else if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+			yv = -MAX_VELOCITY;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+			xv = MAX_VELOCITY;
+			faceRight = true;
+		}
+		else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+			xv = -MAX_VELOCITY;
+			faceRight = false;
+		}
+		yv += GRAVITY;
+
+		float delta = Gdx.graphics.getDeltaTime();
+		y += yv * delta;
+		x += xv * delta;
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+
+		if (y<0) {
+			y = 0;
+			canJump = true;
+		}
+
+	}
+
+	public float decelerate(float velocity){
+		velocity *= DECELERATION;
+		if (Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
 	}
 }
 /*
