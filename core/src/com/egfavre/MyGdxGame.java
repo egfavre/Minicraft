049ee088ec1f37434e8d7d7b18196bc63f75986f
Liz Favre <egfavre@gmail.com>
2016-06-05 14:52:51 -0400

    add rat

--- core/src/com/egfavre/MyGdxGame.java
+++ core/src/com/egfavre/MyGdxGame.java
@@ -11,8 +11,8 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	float time, x, y, xv, yv;
-	TextureRegion down, up, right, left, groundTiles;
+	float time, x, y, xv, yv, ratX, ratXv;
+	TextureRegion down, up, right, left, groundTiles, ratStand;
 	Texture pineTree, sky;
 	static final int WIDTH = 16;
 	static final int HEIGHT = 16;
@@ -23,31 +23,39 @@ public class MyGdxGame extends ApplicationAdapter {
 	static final float DECELERATION = 0.95f;
 	static final int GRAVITY = -50;
 	boolean canJump, faceRight = true;
-	Animation walk;
+	Animation walk, ratAttack;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		Texture tiles = new Texture("tiles.png");
 		Texture ground = new Texture("groundtiles.png");
+		Texture rat = new Texture("CaveRat.png");
 		pineTree = new Texture("PineTree.png");
 		sky = new Texture("Daytime_Background_1024x800.png");
 		TextureRegion[][] grid = TextureRegion.split(tiles, WIDTH, HEIGHT);
 		TextureRegion[][] grid2 = TextureRegion.split(ground, WIDTH, HEIGHT);
+		TextureRegion[][] grid3 = TextureRegion.split(rat, 60, 50);
 		groundTiles = grid2[0][0];
 		down = grid[6][0];
 		up = grid[6][1];
 		right = grid[6][3];
+		ratStand = grid3[0][0];
+		ratStand.flip(true, false);
 		left = new TextureRegion(right);
 		left.flip(true, false);
 		walk = new Animation(0.2f, grid[6][0], grid[6][2]);
+		ratAttack = new Animation(0.2f, grid3[0][2], grid3[0][4], grid3[0][6]);
+
 	}
 
 		@Override
 		public void render () {
 		move();
+		moveRat();
 		time += Gdx.graphics.getDeltaTime();
 		TextureRegion img;
+		TextureRegion attack;
 		wrapScreen();
 
 		if (y>0) {
@@ -60,6 +68,11 @@ public class MyGdxGame extends ApplicationAdapter {
 			img = left;
 		}
 
+		if (ratXv != 0) {
+			ratStand = ratAttack.getKeyFrame(time, true);
+		}
+
+
 		Gdx.gl.glClearColor(0f, .5f, 1f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -68,6 +81,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.draw(groundTiles, 0, 0, FINAL_WIDTH*10, FINAL_HEIGHT*2);
 		batch.draw(pineTree, 50, 50, FINAL_WIDTH*2.5f, FINAL_HEIGHT*2.5f);
 		batch.draw(pineTree, 250, 100, FINAL_WIDTH*2.5f, FINAL_HEIGHT*2.5f);
+		batch.draw(ratStand, 550, 0);
 
 		if (faceRight) {
 			batch.draw(img, x, y, FINAL_WIDTH, FINAL_HEIGHT);
@@ -138,5 +152,12 @@ public class MyGdxGame extends ApplicationAdapter {
 		if (x < -0){
 			x = 600;
 		}
+	}
+
+	public void moveRat(){
+		ratXv = -MAX_VELOCITY;
+		int start = 300;
+		float delta = Gdx.graphics.getDeltaTime();
+		ratX += start + ratXv * delta;
 	}
 }
